c5cbafda427cfcea110302f6190eb7bbd23ca2ab
Egor <egorraybul@gmail.com>
2024-07-14 15:33:07 +0300

    wdf

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    // Run type-check on changes to TypeScript files
-    '**/*.ts?(x)': () => 'npm run lint:fix',
-    // Run ESLint on changes to JavaScript/TypeScript files
-    '**/*.(css|scss)': (filenames) => 'npm run stylelint:fix',
+    '**/*.ts?(x)': () => ['npm run typecheck', 'npm run lint:fix'],
+    '**/*.(css|scss)': () => 'npm run stylelint:fix',
 };
